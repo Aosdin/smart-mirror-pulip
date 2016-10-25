02f21df9e19a1e0b4597389e80bc4a5ac4d95c30
Aosdin <schemak@gmail.com>
2016-10-25 17:25:28 +0900

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+var FORCAST_API_KEY = "{}";
+var SUBWAY_API_KEY = "{}";
+var SOUNDCLOUD_API_KEY = "{}";
+var YOUTUBE_API_KEY = "{AIzaSyB2gvK3JLiAJz_OSrm_1_TIjZNcCXxw7fU}";
+var HUE_BASE = "http://192.168.0.36/api/{}/";
